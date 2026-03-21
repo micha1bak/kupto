b84b28ba5bd26d572d40b3f34ac8ea2ab383de93
Michał Bąk <michalbak000@gmail.com>
2026-03-21 13:50:46 +0100

    middleware.ts -> proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -4,7 +4,7 @@ import { jwtVerify } from 'jose';
 
 const secret = new TextEncoder().encode(process.env.JWT_SECRET);
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const token = request.cookies.get('token')?.value;
 
   // Ścieżki publiczne
