f1549a3e0b036dfb1086cd4582106e1a3249bfef
Michał Bąk <michalbak000@gmail.com>
2026-04-01 11:08:02 +0200

    created /api/register endpoint that uses createUser utility

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,9 +1,26 @@
 import express from 'express'
-import { prisma } from './db';
+import createUser from "./utils/createUser";
 
 const app = express();
 
-app.use(express.static('src/frontend'))
+app.use(express.json(), express.static('src/frontend'))
+
+app.post('/api/register', async (req, res) => {
+    const { login, password } = req.body;
+    if (!login || !password) {
+        return res.status(400).send({'error': 'Login and Password expected!'});
+    }
+    try {
+        await createUser({ login, password });
+        return res.status(201).json({ message: 'User created successfully.' });
+    } catch (error: any) {
+        if (error.code === 'P2002') {
+            return res.status(409).json({ error: 'Login is already taken.' });
+        }
+        console.error(error);
+        return res.status(500).json({ error: 'Internal server error.' });
+    }
+})
 
 app.listen(3000, () => {
     console.log('App running on http://localhost:3000');
